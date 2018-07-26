41c097400b44ae6a7f3592ba189a3ce0261817cf
zenithpolar <zenithpolar2@gmail.com>
2018-07-26 17:58:35 -0300

    price implementation

--- contracts/CryptoDoggies.sol
+++ contracts/CryptoDoggies.sol
@@ -109,20 +109,94 @@ contract CryptoDoggies is AccessControl, DetailedERC721{
     }
 
     function tokensOf(address _owner) public view returns(uint256[]){
-       uint256 tokenCount = balanceOf(_owner);
-       if(tokenCount == 0){
-           return new uint256[](0);
-       } else {
-           uint256[] memory result = new uint256[](tokenCount);
-           uint256 total = totalSupply();
-           uint resultIndex = 0;
-           
-           for(uint256 i=0; i< total; i++){
-               if(tokenIdtoOwner[i] == _owner){
-                   result[resultIndex];
-                   resultIndex++;
-               }
-           }
-       }
+        uint256 tokenCount = balanceOf(_owner);
+        if(tokenCount == 0){
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 total = totalSupply();
+            uint resultIndex = 0;
+
+            for(uint256 i=0; i< total; i++){
+                if(tokenIdtoOwner[i] == _owner){
+                    result[resultIndex];
+                    resultIndex++;
+                }
+            }
+        }
+    }
+
+    function withdrawBalance(address _to, uint256 _amount) public onlyCEO{
+        require(_amount <= this.balance);
+
+        if (_amount == 0){
+            _amount = this.balance;
+        }
+        if (_to == address(0)){
+            ceoAddress.transfer(_amount);
+        } else {
+            _to.transfer(_amount);
+        }
+    }
+
+    function purchase(uint256 _tokenId) public payable whenNotPaused {
+        address oldOwner = ownerOf(_tokenId);
+        address newOwner = msg.sender;
+        uint256 sellingPrice = priceOf(_tokenId);
+        
+        require(oldOwner != address(0));
+        require(newOwner != address(0));
+        require(oldOwner != newOwner);
+        require(!_isContract(newOwner));
+        require(sellingPrice > 0);
+        
+        transfer(oldOwner, newOwner, _tokenId);
+        TokenSold(
+            _tokenId,
+            doggies[_tokenId].name,
+            doggies[_tokenId].dna,
+            sellingPrice,
+            priceOf(_tokenId),
+            oldOwner,
+            newOwner
+        );
+
+        uint256 excess = msg.value.sub(sellingPrice);
+        uint256 contractCut = sellingPrice.mul(6).div(100);//6% cut
+
+        if(oldOwner != address(this)) {
+            oldOwner.transfer(sellingPrice.sub(contractCut));
+        }
+
+        if(excess > 0){
+            newOwner.transfer(excess);
+        }
+    }
+
+    function priceOf(uint256 _tokenId) public view returns (uint256 _price) {
+        return tokenIdToPrice[_tokenId];
+    }
+
+    uint256 private increaseLimit1 = 0.02 ether;
+    uint256 private increaseLimit2 = 0.5 ether;
+    uint256 private increaseLimit3 = 2.0 ether;
+    uint256 private increaseLimit4 = 5.0 ether;
+
+    function nextPriceOf(uint256 _tokenId) public view returns (uint256 _nextPrice){
+        uint256 _price = priceOf(_tokenId);
+        if(_price < increaseLimit1){
+            return _price.mul(200).div(95);
+        } else if(_price < increaseLimit2){
+            return _price.mul(135).div(96);
+        } else if(_price < increaseLimit3){
+            return _price.mul(125).div(97);
+        } else if(_price < increaseLimit4){
+            return _price.mul(117).div(97);
+        }
+        return _price.mul(115).div(98);
+    }
+
+    function enableERC721() public onlyCEO{
+        erc721Enabled = true;
     }
 }
